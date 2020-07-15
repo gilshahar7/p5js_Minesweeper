ba191a3d4846576145bdad402c8e58cc17f46820
gilshahar7 <gilshahardex99@gmail.com>
2020-07-15 19:27:20 +0300

    Fix pool revealing with MMB

--- tile.js
+++ tile.js
@@ -166,82 +166,114 @@ class Tile{
 
   revealNeighbors(){
     if(this.x != 0){
-      if(board[this.x-1][this.y].isFlagged == false){
-        if(board[this.x-1][this.y].isBomb){
+      var tile = board[this.x-1][this.y];
+      if(tile.isFlagged == false){
+        if(tile.isBomb){
           gameState = 0;
           gameOver();
         }else{
-          board[this.x-1][this.y].isRevealed = true;
+          if(tile.face == 0){
+            tile.revealPool();
+          }
+          tile.isRevealed = true;
         }
       }
     }
     if(this.x != cols-1){
-      if(board[this.x+1][this.y].isFlagged == false){
-        if(board[this.x+1][this.y].isBomb){
+      var tile = board[this.x+1][this.y];
+      if(tile.isFlagged == false){
+        if(tile.isBomb){
           gameState = 0;
           gameOver();
         }else{
-          board[this.x+1][this.y].isRevealed = true;
+          if(tile.face == 0){
+            tile.revealPool();
+          }
+          tile.isRevealed = true;
         }
       }
     }
     if(this.y != 0){
-      if(board[this.x][this.y-1].isFlagged == false){
-        if(board[this.x][this.y-1].isBomb){
+      var tile = board[this.x][this.y-1];
+      if(tile.isFlagged == false){
+        if(tile.isBomb){
           gameState = 0;
           gameOver();
         }else{
-          board[this.x][this.y-1].isRevealed = true;
+          if(tile.face == 0){
+            tile.revealPool();
+          }
+          tile.isRevealed = true;
         }
       }
     }
     if(this.y != rows-1){
-      if(board[this.x][this.y+1].isFlagged == false){
-        if(board[this.x][this.y+1].isBomb){
+      var tile = board[this.x][this.y+1];
+      if(tile.isFlagged == false){
+        if(tile.isBomb){
           gameState = 0;
           gameOver();
         }else{
-          board[this.x][this.y+1].isRevealed = true;
+          if(tile.face == 0){
+            tile.revealPool();
+          }
+          tile.isRevealed = true;
         }
       }
     }
     if (this.x != 0 && this.y != 0) {
-      if(board[this.x-1][this.y-1].isFlagged == false){
-        if(board[this.x-1][this.y-1].isBomb){
+      var tile = board[this.x-1][this.y-1];
+      if(tile.isFlagged == false){
+        if(tile.isBomb){
           gameState = 0;
           gameOver();
         }else{
-          board[this.x-1][this.y-1].isRevealed = true;
+          if(tile.face == 0){
+            tile.revealPool();
+          }
+          tile.isRevealed = true;
         }
       }
     }
     if (this.x != cols-1 && this.y != 0) {
-      if(board[this.x+1][this.y-1].isFlagged == false){
-        if(board[this.x+1][this.y-1].isBomb){
+      var tile = board[this.x+1][this.y-1];
+      if(tile.isFlagged == false){
+        if(tile.isBomb){
           gameState = 0;
           gameOver();
         }else{
-          board[this.x+1][this.y-1].isRevealed = true;
+          if(tile.face == 0){
+            tile.revealPool();
+          }
+          tile.isRevealed = true;
         }
       }
     }
     if (this.x != 0 && this.y != rows-1) {
-      if(board[this.x-1][this.y+1].isFlagged == false){
-        if(board[this.x-1][this.y+1].isBomb){
+      var tile = board[this.x-1][this.y+1];
+      if(tile.isFlagged == false){
+        if(tile.isBomb){
           gameState = 0;
           gameOver();
         }else{
-          board[this.x-1][this.y+1].isRevealed = true;
+          if(tile.face == 0){
+            tile.revealPool();
+          }
+          tile.isRevealed = true;
         }
       }
     }
     if (this.x != cols-1 && this.y != rows-1) {
-      if(board[this.x+1][this.y+1].isFlagged == false){
-        if(board[this.x+1][this.y+1].isBomb){
+      var tile = board[this.x+1][this.y+1];
+      if(tile.isFlagged == false){
+        if(tile.isBomb){
           gameState = 0;
           gameOver();
         }else{
-          board[this.x+1][this.y+1].isRevealed = true;
+          if(tile.face == 0){
+            tile.revealPool();
+          }
+          tile.isRevealed = true;
         }
       }
     }
